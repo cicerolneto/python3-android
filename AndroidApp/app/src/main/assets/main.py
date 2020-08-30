b77e48d4bdbbcace22e14769b8b1b8e174fb3f54
Brian Spratke <brian.spratke@dematic.com>
2020-08-30 09:47:17 -0400

    This is a quick update to make sure 3.6 can output StdErr and StdOut.

--- AndroidApp/app/src/main/assets/main.py
+++ AndroidApp/app/src/main/assets/main.py
@@ -1,10 +1,18 @@
 
 def main():
     import os
+    print(os.__file__)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     out_file = '{}/HelloWorld.txt'.format(dir_path)
     with open(out_file, 'a') as the_file:
         the_file.write('It works!\n')
+
+    import decimal
+    print(decimal.__file__)
+
+    with open(out_file, 'a') as the_file:
+        the_file.write('It Still works!\n')
+    print('Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 
 if __name__ == '__main__':
